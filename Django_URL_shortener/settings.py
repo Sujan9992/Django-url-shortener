e699bbc46069e962b7ceb8535e6360caffdfb96b
Sujan Thapa <66910924+Sujan9992@users.noreply.github.com>
2021-06-18 16:02:00 +0545

    Update settings.py
    Secret Key removed

--- Django_URL_shortener/settings.py
+++ Django_URL_shortener/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5ca-g=m4x0+0zdwi=rpvk!5dln3z3ha!td$mr4==b+=m&@o2*'
+SECRET_KEY = 'Your secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
